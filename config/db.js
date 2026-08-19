0587a33c63090566ce1f5e7c8acc424e39070c47
Ruy Goes <ruygoes.dev@gmail.com>
2026-08-19 14:25:31 -0300

    Configuração do banco de dados

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import "dotenv/config"
+
+const db = new Pool({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+})
+
+export default db;
