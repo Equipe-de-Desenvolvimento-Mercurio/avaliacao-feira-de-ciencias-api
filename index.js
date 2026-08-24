5493b645a8a11eb50338dd63372836792598140b
ruygoesdev <ruygoes.dev@gmail.com>
2026-08-24 19:56:55 -0300

    Configuração Express

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
+import authRoute from "./routes/auth.js"
 
 const app = express();
 app.use(express.json());
 
+// Endpoint de login
+app.use("/auth", authRoute);
 
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Executando...")
+});
