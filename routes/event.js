ae5abbcbadf314cf7ee890db8a5563f338229ce9
Ruy <ruygoes.dev@gmail.com>
2026-08-27 16:21:40 -0300

    Criar relação evento professor

--- routes/event.js
+++ routes/event.js
@@ -63,6 +63,38 @@ router.get("/:id", async (req, res) => {
 
 });
 
+// Dentinar Evento para Professor
+router.post("/usuario/:id_professor/:id_evento", async (req, res) => {
+    let { id_professor, id_evento } = req.params;
+
+    let usuario = await pool.query("SELECT * FROM usuario WHERE id_usuario = $1", [id_professor]);
+    if (usuario.rowCount == 0){
+        return res.status(404).json({
+            error: "Usuario não encontrado"
+        })
+    }
+
+    let evento = await pool.query("SELECT * FROM evento WHERE id_evento = $1", [id_evento]);
+    if (evento.rowCount == 0){
+        return res.status(404).json({
+            error: "Evento não encontrado"
+        })
+    }
+
+    let participacao = await pool.query("SELECT * FROM participacao_evento WHERE id_usuario = $1 AND id_evento = $2", [id_professor, id_evento]);
+    if (participacao.rowCount != 0){
+        return res.status(400).json({
+            error: "Participação já existe"
+        })
+    }
+
+    let data = await pool.query("INSERT INTO participacao_evento (id_usuario, id_evento) VALUES ($1, $2) RETURNING *", [id_professor, id_evento]);
+
+    return res.status(201).json({
+        message: "Participação adicionada com sucesso"
+    });
+});
+
 
 function verificarData(date){
     if (typeof date !== "string" || !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
