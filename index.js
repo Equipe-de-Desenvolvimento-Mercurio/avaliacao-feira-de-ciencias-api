389b01217c2c07397761d134f36be42496b073e3
Isa <isabelinavaz1218@gmail.com>
2026-09-01 15:03:40 -0300

    configuracao endppoint projetos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoute from "./routes/auth.js"
 import eventRoute from "./routes/event.js"
+import projectRoute from "./routes/project.js"
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.json());
 // Endpoint de login
 app.use("/auth", authRoute);
 app.use("/event", eventRoute);
+app.use("/project", eventRoute);
 
 app.listen(3000, () => {
     console.log("Executando...")
